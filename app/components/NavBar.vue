0867cbd2885e91fa1f19972b7bda4a54038be536
shwephuehmone25 <shwephue7889@gmail.com>
2025-02-27 10:31:27 +0630

    add flight menu in navbar

---
+++ app/components/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+      <NuxtLink to="/" class="flex gap-1 items-center">
+        <NuxtImg width="56px" src="/fly.png" format="webp" alt="" />
+        <span class="text-3xl font-bold">Wonderpackage</span>
+      </NuxtLink>
+      <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/flights">Flights</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/login">Login</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/register" >Register</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
