cb5fe98e62cd11be179c45727a39e9227fc13762
shwephuehmone25 <shwephue7889@gmail.com>
2025-02-28 22:58:09 +0630

    get all flights

--- app/components/FlightCard.vue
+++ app/components/FlightCard.vue
@@ -7,30 +7,34 @@ defineProps<{
 </script>
 
 <template>
-    <div v-if="flight" class="flex flex-col shadow rounded-md mt-3">
-        <div class="flex flex-col py-6 px-4 flex-1">
-            <!-- Flight Destination -->
-            <p class="text-xl lg:text-2xl font-semibold mb-2">{{ flight.destination }}</p>
-            <p class="text-lg text-gray-600">{{ flight.airline }}</p>
-
-            <!-- Flight Details -->
-            <div class="font-normal w-full bg-white/80 flex gap-8 text-lg lg:text-xl mb-4 mt-auto">
-                <div class="flex items-center gap-1">
-                    <UIcon name="i-mdi-airplane-takeoff" class="text-blue-500" />
-                    <span>Departure: {{ flight.departureTime }}</span>
-                </div>
-                <div class="flex items-center gap-1">
-                    <UIcon name="i-mdi-airplane-landing" class="text-blue-500" />
-                    <span>Arrival: {{ flight.arrivalTime }}</span>
-                </div>
-                <div class="flex items-center gap-1">
-                    <UIcon name="i-mdi-cash" class="text-blue-500" />
-                    <span>{{ flight.price }}</span>
+    <div class="w-full" v-if="flight">
+        <div class="swiper swiper-initialized swiper-horizontal" clickable="true">
+            <div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px)">
+                <div class="swiper-slide swiper-slide-active swiper-slide-next"
+                    style="width: 479px">
+                    <div
+                        class="relative group cursor-pointer w-full shadow-md border rounded-lg overflow-hidden group hover:shadow-lg duration-300 ease-in-out">
+                        <div class="overflow-hidden">
+                            <img class="w-full h-[100px] md:h-[190px] object-cover group-hover:scale-[1.1] duration-300"
+                                :src="flight.image" />
+                        </div>
+                        <div class="w-full">
+                            <div class="px-3 py-1 md:px-5 md:py-3 relative flex flex-col gap-2 justify-between">
+                                <div>
+                                    <small class="text-[10px] sm:text-sm md:text-lg">{{ flight.airline }}</small><small
+                                        class="block text-[10px] md:text-lg mt-2"><i
+                                            class="fa-solid fa-plane opacity-50"></i>🛬{{ flight.departure_airport
+                                        }}</small><span
+                                        class="text-[9px] md:text-lg flex flex-col items-end w-full"><span
+                                            class="block text-[7px] md:text-sm">Start From </span><span
+                                            class="text-[10px] md:text-lg">THB
+                                            <span class="text-[10px] md:text-lg">{{ flight.price }}</span></span></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <!-- Button to View Flight -->
-            <UButton :ui="{ base: 'self-start' }" :to="`/flights/${flight.id}`" label="View" />
         </div>
     </div>
 </template>
